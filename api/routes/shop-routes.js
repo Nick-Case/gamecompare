17067bde19c95322909e917f8565eda6cfef5a49
AllFallD0wn <nickcase@outlook.com>
2019-06-27 13:42:25 +0100

    added shop-routes

---
+++ api/routes/shop-routes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const shops = require("../controllers/shop-controller");
+
+const router = express.Router();
+
+function respondGetAll(res) {
+
+    shops.getAll()
+    .then(s => {
+        res.json(s)
+    })
+    .catch(() => {
+        res.status(500).json({error: "Couldn't get all shops"});
+    });
+}
+
+function respondCreate(req, res) {
+    shops.create(req.body)
+    .then(shop => {
+        res.status(201).json(shop);
+    })
+    .catch(err => {
+        res.status(500).json({error : "Error"})
+    });
+}
+router.get('/', (req, res) => {
+    respondGetAll(res);
+})
+
+router.post('/', (req, res) => {
+    respondCreate(req, res);
+})
+
+module.exports = router;
